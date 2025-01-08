f3b29c675a3771122e080db3b33bd24b59f8ec97
noreply <noreply.snet@gmail.com>
2025-01-08 19:07:01 +0530

    -imp set all atms api

--- app/apis/atm_api.py
+++ app/apis/atm_api.py
@@ -1,23 +1,28 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from ..database.session import get_db
-from ..cruds.atm_crud import create_atm, read_atm, update_atm, delete_atm
-from ..schemas.atm_schemas import AtmDataCreate, AtmDataUpdate, AtmDataRead
+from ..cruds.atm_crud import create_atm, read_atm, update_atm, delete_atm, read_all_atms
+from ..schemas.atm_schemas import AtmDataCreate, AtmDataUpdate
+from typing import List
 
 router = APIRouter()
 
-@router.post("/", response_model=AtmDataRead)
+@router.post("/", response_model=AtmDataUpdate)
 def create_atm_route(atm: AtmDataCreate, db: Session = Depends(get_db)):
     return create_atm(db, atm)
 
-@router.get("/{atm_id}", response_model=AtmDataRead)
+@router.get("/all", response_model=List[AtmDataUpdate])
+def read_atm_route( db: Session = Depends(get_db)):
+    return read_all_atms(db)
+
+@router.get("/{atm_id}", response_model=AtmDataUpdate)
 def read_atm_route(atm_id: int, db: Session = Depends(get_db)):
     return read_atm(db, atm_id)
 
-@router.put("/{atm_id}", response_model=AtmDataRead)
-def update_atm_route(atm_id: int, atm: AtmDataUpdate, db: Session = Depends(get_db)):
-    return update_atm(db, atm_id, atm)
+@router.put("/", response_model=AtmDataUpdate)
+def update_atm_route(atm: AtmDataUpdate, db: Session = Depends(get_db)):
+    return update_atm(db, atm)
 
-@router.delete("/{atm_id}", response_model=AtmDataRead)
+@router.delete("/{atm_id}")
 def delete_atm_route(atm_id: int, db: Session = Depends(get_db)):
     return delete_atm(db, atm_id)
--- app/cruds/atm_crud.py
+++ app/cruds/atm_crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-from fastapi import HTTPException
+from fastapi import HTTPException , status
+from fastapi.responses import JSONResponse
 from ..models.main_models import AtmDataModel
 from ..schemas.atm_schemas import AtmDataCreate, AtmDataUpdate
 
@@ -10,14 +11,20 @@ def create_atm(db: Session, atm: AtmDataCreate):
     db.refresh(db_atm)
     return db_atm
 
+def read_all_atms(db: Session):
+    db_atm = db.query(AtmDataModel).all()
+    if not db_atm:
+        raise HTTPException(status_code=404, detail="ATMs data not found")
+    return db_atm
+
 def read_atm(db: Session, atm_id: int):
     db_atm = db.query(AtmDataModel).filter(AtmDataModel.id == atm_id).first()
     if not db_atm:
         raise HTTPException(status_code=404, detail="ATM data not found")
     return db_atm
 
-def update_atm(db: Session, atm_id: int, atm: AtmDataUpdate):
-    db_atm = db.query(AtmDataModel).filter(AtmDataModel.id == atm_id).first()
+def update_atm(db: Session, atm: AtmDataUpdate):
+    db_atm = db.query(AtmDataModel).filter(AtmDataModel.id == atm.id).first()
     if not db_atm:
         raise HTTPException(status_code=404, detail="ATM data not found")
     for key, value in atm.model_dump(exclude_unset=True).items():
@@ -32,4 +39,7 @@ def delete_atm(db: Session, atm_id: int):
         raise HTTPException(status_code=404, detail="ATM data not found")
     db.delete(db_atm)
     db.commit()
-    return db_atm
+    return JSONResponse(
+        content={"msg": f"ATM data with ID {atm_id} has been deleted successfully."},
+        status_code=status.HTTP_200_OK,
+    )
